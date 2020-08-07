28ca4301b632a0895ead990090c0b0860a24f8bf
rnjshippo <tkstka0023@naver.com>
2020-08-08 01:34:27 +0900

    keywords 추가
    keywords에 글 내용, 나이, 성별 ... 정보 일단 넣어놓음

--- models/posts.js
+++ models/posts.js
@@ -1,24 +1,22 @@
 const pool = require('../lib/database');
 
-// TODO :: sqlite3로 쿼리 날리기
-
 /* 전체 글 조회 */
 const getAllPosts = () => {
   let sql = "SELECT * FROM Board;";
   return pool.execute(sql);
-  // return sqlite3 
 }
 
 /* 글 작성  */
 const createPost = (id, gender, age, info, start_date, end_date, place, group_name, story) => {
-  let sql = `INSERT into Board (id, gender, age, info, start_date, end_date, place, group_name, story) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`;
-  return pool.execute(sql, [id, gender, age, info, start_date, end_date, place, group_name, story]);
-  // return sqlite3
+  // TODO :: 일단 keywords 대신에 모든 정보를 넣었음 , 추후 형태소 분석 구현시 대체할 것
+  let sql = `INSERT into Board (id, gender, age, info, start_date, end_date, place, group_name, story, keywords) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+  return pool.execute(sql, [id, gender, age, info, start_date, end_date, place, group_name, story,
+    `${story} ${info} ${age ? age + "세" : ""} ${gender} ${start_date} ${end_date} ${place} ${group_name}`]);
 }
 
 /* 키워드로 게시판 글 검색 */
 const getPostsByKeyword = (keyword) => {
-  let sql = `SELECT * FROM Board WHERE keywords like '%${keyword}%';`;; // TODO :: sql 
+  let sql = `SELECT * FROM Board WHERE keywords like '%${keyword}%';`;
   return pool.execute(sql);
 }
 
