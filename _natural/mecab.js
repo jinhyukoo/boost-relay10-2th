a2d656a35759095fc07d5f91ed7a0a53a88fc709
rnjshippo <tkstka0023@naver.com>
2020-08-08 12:08:50 +0900

    Update mecab.js

--- _natural/mecab.js
+++ _natural/mecab.js
@@ -30,5 +30,5 @@ mecab.nouns(text, function (err, result) {
 (async () => {
   const { id, gender, age, info, start_date, end_date, place, group_name, story } = req.body;
   const [err, result] = await mecab.nouns([id, gender, age, info, start_date, end_date, place, group_name, story].join(" "));
-  console.log(err, result)
+  console.log("result", result)
 })()
